8be1b473dde7b2229d3b569f3232a4444150e779
SUraj Negi <er.surajnegi@gmail.com>
2018-02-10 00:51:07 +0530

    DemoCode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef } from '@angular/core';
+declare var window: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+        selector: 'app-root',
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+        title = 'app';
+        speech: any;
+        textValue: any = '';
+        constructor(private _cdRef: ChangeDetectorRef) {
+                try {
+                        let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+                        this.speech = new SpeechRecognition();
+                        this.initEvents();
+                }
+                catch (e) {
+                        console.error(e);
+                }
+        }
+        initEvents() {
+                this.speech.onresult = (event) => {
+                        const current = event.resultIndex;
+                        this.textValue = event.results[current][0].transcript;
+                        this._cdRef.detectChanges();
+                        this.readOutLoud(this.textValue);
+                };
+        }
+        startRecording() {
+                this.speech.start();
+        }
+        stop() {
+                this.speech.stop();
+        }
+        readOutLoud(message) {
+                try {
+                        const speech = new SpeechSynthesisUtterance();
+
+                        // Set the text and voice attributes.
+                        speech.text = message;
+                        speech.volume = 1;
+                        speech.rate = 1;
+                        speech.pitch = 1;
+
+                        window.speechSynthesis.speak(speech);
+                }
+        }
 }
